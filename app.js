6870e2ee018450883d754c284b93ef49cdb7b94b
Tushar-2602 <tkumar2602@gmail.com>
2025-07-17 22:29:29 +0530

    .

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 import express from "express";
 import cors from "cors";
 const app=express();
-app.use(cors());
+app.use(cors(
+  {
+  origin: '*'
+  }
+));
 app.use(express.json()); // For parsing application/json
 app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
 
 import { api_router } from "./route.js";
 app.use('',api_router);
-app.use((req, res, next) => {
-  res.status(404).send("404 server not found");
-});
+// app.use((req, res, next) => {
+//   res.status(404).send("404 server not found");
+// });
 export {
     app
 }
